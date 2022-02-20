26d38ddefc91d91fc428002e41e2488d4a48357c
Darshil <darsheelsheth@yahoo.com>
2022-02-20 16:28:24 +0530

    email shifted to prod

--- BEAUTIFULHOMES/settings.py
+++ BEAUTIFULHOMES/settings.py
@@ -148,14 +148,6 @@ MESSAGE_TAGS = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email Config
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shethdarsheel@gmail.com'
-EMAIL_HOST_PASSWORD = 'Darshils@1'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
